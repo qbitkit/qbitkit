ed4e505a35729b0ccbfaf62e637130111f7fe46c
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-19 15:31:43 -0800

    try refactor

--- qbitkit/provider/braket/provider.py
+++ qbitkit/provider/braket/provider.py
@@ -1,11 +1,3 @@
-import itertools
-from itertools import combinations
-from dwave.system.composites import EmbeddingComposite
-from braket.ocean_plugin import BraketDWaveSampler
-from datetime import datetime as dt
-import pandas as pd
-import dimod
-from math import log2, floor
 from circuit import circuitry
 
 from braket.aws import AwsDevice
