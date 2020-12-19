33d335e7ca5c81a43202eb3897ff00e103349ce7
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-19 13:44:58 -0800

    fixed incomplete function

--- src/qbitkit/circuitry/gates.py
+++ src/qbitkit/circuitry/gates.py
@@ -3,7 +3,8 @@ from ..error import error as e
 class single_qubit:
     def __init__(self):
         df = f.get_frame()
-    def gate(df=f.get_frame(),
+    def gate(append=False,
+             append_df=f.get_frame(),
              gate='h',
              target='0'):
         gate_data = {
@@ -11,3 +12,10 @@ class single_qubit:
             'gate': gate,
         }
         gate_frame = f.get_frame(data=gate_data)
+        if append == True:
+            appended_df = append_df.append(gate_frame)
+            return appended_df
+        elif append == False:
+            return gate_frame
+        else:
+            return gate_frame
