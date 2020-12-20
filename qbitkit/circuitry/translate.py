9707f0b402d97c33953cdef62a02107600f2cdab
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-19 19:46:40 -0800

    restoring after fix
    lost changes to .gitignore, reverted to get the changes back, now commiting the lost changes

--- qbitkit/circuitry/translate.py
+++ qbitkit/circuitry/translate.py
@@ -1,1 +1,14 @@
-from ..provider.braket.provider import circuitry as braket_circuitry
+from ..provider.braket.circuit.circuitry import translate as translate_braket
+from ..provider.braket.circuit.circuitry import braket_circuit as braket_circuit
+class to_provider:
+    def braket(op=None,
+               input_circuit=braket_circuit(),
+               targetA=None,
+               targetB=None,
+               targetC=None):
+        translated = translate_braket.translate_instruction(op,
+                                                            input_circuit,
+                                                            targetA,
+                                                            targetB,
+                                                            targetC)
+        return translated
--- qbitkit/provider/braket/circuit/circuitry.py
+++ qbitkit/provider/braket/circuit/circuitry.py
@@ -1,4 +1,5 @@
-from braket.circuits import Circuit, Gate
+from braket.circuits import Circuit as braket_circuit
+from braket.circuits import Gate as braket_gate
 import string
 class info:
         def get_gates(self):
@@ -6,10 +7,20 @@ class info:
             return gate_set
 class translate:
     def translate_instruction(op=None,
-                              input_circuit=Circuit(),
+                              input_circuit=braket_circuit(),
                               targetA=0,
                               targetB=0,
                               targetC=0):
-        if op == 'toffoli':
-            input_circuit = input_circuit.ccnot()
+        if op == 'h':
+            input_circuit = input_circuit.h(targetA)
+        if op == 'cnot':
+            input_circuit = input_circuit.cnot(targetA,
+                                               targetB)
+        if op == 'ccnot':
+            input_circuit = input_circuit.ccnot(targetA,
+                                                targetB,
+                                                targetC)
+        else:
+            print(f'[ERROR]: Gate {op} not found. Returning an empty object.')
+            input_circuit = None
         return input_circuit
