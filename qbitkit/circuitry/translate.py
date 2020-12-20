f7ea5e16dae8e4d1b801dc74cb757aaab3ea5031
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-19 21:56:12 -0800

    fix default values in translator

--- qbitkit/circuitry/translate.py
+++ qbitkit/circuitry/translate.py
@@ -6,9 +6,15 @@ class to_provider:
                targetA=None,
                targetB=None,
                targetC=None):
-        translated = translate_braket.translate_gate(op,
-                                                     input_circuit,
-                                                     targetA,
-                                                     targetB,
-                                                     targetC)
+        translated = translate_braket.translate_gate(op=None,
+                                                     input_circuit=braket_circuit(),
+                                                     targetA=0,
+                                                     targetB=1,
+                                                     targetC=2,
+                                                     angle=0.15,
+                                                     phi=0.15,
+                                                     theta=0.15,
+                                                     unitary_matrix=np.array([[0,1],
+                                                                              [1,0]]),
+                                                     unitary_targets=[0])
         return translated
