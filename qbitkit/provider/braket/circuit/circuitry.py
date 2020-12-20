8d1fcad840545594c309c2ffdae4b71a6c92c374
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-19 19:39:30 -0800

    translation & braket circuitry

--- qbitkit/provider/braket/circuit/circuitry.py
+++ qbitkit/provider/braket/circuit/circuitry.py
@@ -1,15 +1,26 @@
-from braket.circuits import Circuit, Gate
+from braket.circuits import Circuit as braket_circuit
+from braket.circuits import Gate as braket_gate
 import string
 class info:
         def get_gates(self):
-            gate_set = [attr for attr in dir(Gate) if attr[0] in string.ascii_uppercase]
+            gate_set = [attr for attr in dir(braket_gate) if attr[0] in string.ascii_uppercase]
             return gate_set
 class translate:
     def translate_instruction(op=None,
-                              input_circuit=Circuit(),
+                              input_circuit=braket_circuit(),
                               targetA=0,
                               targetB=0,
                               targetC=0):
-        if op == 'toffoli':
-            input_circuit = input_circuit.ccnot()
+        if op == 'h':
+            input_circuit = input_circuit.h(targetA)
+        if op == 'cnot':
+            input_circuit = input_circuit.cnot(targetA,
+                                               targetB)
+        if op == 'ccnot':
+            input_circuit = input_circuit.ccnot(targetA,
+                                                targetB,
+                                                targetC)
+        else:
+            print(f'[ERROR]: Gate {op} not found. Returning an empty object.')
+            input_circuit = None
         return input_circuit
