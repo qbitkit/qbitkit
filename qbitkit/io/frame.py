07035a1db2a5e55cfc704dd887871b65e4a65e65
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-20 16:31:35 -0800

    add save_frame fn to frame.py

--- qbitkit/io/frame.py
+++ qbitkit/io/frame.py
@@ -1,5 +1,18 @@
 import modin.pandas as pd
+import file as f
 class frame:
     def get_frame(data=None):
         df = pd.DataFrame(data=data)
         return df
+    def save_frame(frame=get_frame(),
+                   pth='data.csv',
+                   file_type='csv'):
+        if file_type == 'csv':
+            f.excel.write(data_path=pth,
+                        df=frame)
+        if file_type == 'json':
+            f.excel.write(data_path=pth,
+                        df=frame)
+        if file_type == 'excel':
+            f.excel.write(data_path=pth,
+                        df=frame)
