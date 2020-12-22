568831de265385b314e42921915829d89fef74f8
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-21 16:16:30 -0800

    fix imports in translate.py

--- qbitkit/circuitry/translate.py
+++ qbitkit/circuitry/translate.py
@@ -1,5 +1,5 @@
-from ..provider.braket.circuit.circuitry import translate as translate_braket
-from ..provider.braket.circuit.circuitry import braket_circuit as braket_circuit
+from qbitkit.provider.braket.circuit.circuitry import translate as translate_braket
+from qbitkit.provider.braket.circuit.circuitry import braket_circuit as braket_circuit
 class to_provider:
     def braket(op=None,
                input_circuit=braket_circuit(),
