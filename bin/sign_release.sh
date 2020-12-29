aef9466940340e4944895976ede6073989b167ea
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-28 20:06:17 -0800

    avoid aliasing in release signer

--- bin/sign_release.sh
+++ bin/sign_release.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 # This is supposed to be run in a DispVM with no network access, and takes advantage of the Qubes GPG proxy to ensure the highest level of security.
 # More important than anything else is MAKING SURE TO USE SMART CARDS as this is the only way to effectively defend against malware stealing your private key(s)
-export gpgBinary=qubes-gpg-client
-alias crypt=gpgBinary
-
 export filename=$1.sig.asc
 
 echo "Signing file $1 with $gpgBinary and saving to $filename"
-cat $1 | crypt --sign --armor > $filename
+cat $1 | qubes-gpg-client --sign --armor > $filename
 
 echo "Detached Signature for file $1:"
 cat $filename
