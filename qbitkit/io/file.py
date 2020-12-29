023c02d1fe23d93925d594e05450d9144f0200d4
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-28 19:14:02 -0800

    Document all of io.file
    pursuant issue #6
    
    document all functions contained in qbitkit.io.file

--- qbitkit/io/file.py
+++ qbitkit/io/file.py
@@ -3,39 +3,67 @@ import os as pyos
 
 class csv:
     def read(data_path=None):
+        """Read a CSV from a specified path.
+
+        Keyword arguments:
+        data_path -- specify a path to read CSV data from. (default None)"""
         df = pd.read_csv(data_path)
         return df
     def write(data_path=None,
               df=pd.DataFrame()):
+        """Write a CSV to a specified path.
+
+        Keyword arguments:
+        data_path -- specify a path to write the CSV data to. (default None)"""
         df.to_csv(data_path)
         return data_path
 
 class json:
     def read(data_path=None):
+        """Read JSON data from a file at a specified path.
+
+        Keyword arguments:
+        data_path -- specify a path to read JSON data from. (default None)"""
         df = pd.read_json(data_path)
         return df
 
     def write(data_path=None,
               df=pd.DataFrame()):
+        """Write JSON data to a file at a specified path.
+
+        Keyword arguments:
+        data_path -- specify a path to write JSON data to. (default None)"""
         df.to_json(data_path)
         return data_path
 
 class excel:
     def read(data_path=None):
+        """Read Microsoft Office Excel data from a file at a specified path.
+
+        Keyword arguments:
+        data_path -- specify a path to read Microsoft Office Excel data from. (default None)"""
         df = pd.read_excel(data_path)
         return df
     def write(data_path=None,
               df=pd.DataFrame()):
+        """Write Microsoft Office Excel data to a file at a specified path.
+
+        Keyword arguments:
+        data_path -- specify a path to write Microsoft Office Excel data to. (default None)"""
         df.to_(data_path)
         return data_path
 
 def file(direction='read',
          directory=pyos.getcwd(),
-         filename=None,
-         fileType='csv',
-         df=pd.DataFrame(),
-         fileExt=None):
+         filename='data',
+         fileType='csv'):
+    """High-level function for both reading and writing files.
 
+    Keyword arguments:
+    direction -- specify whether to 'read' or 'write' data. (default 'read')
+    directory -- specify a directory from which to build a path to save a file to. (default pyos.getcwd())
+    filename -- specify a filename to save the file to. Will be appended to the directory to form a path without an extension. (default 'data')
+    fileType -- specify the type of file you wish to save the file in. choose from 'excel', 'csv' or 'json'. (default 'csv')"""
     if filename == None:
         print('ERROR: No file name specified. Please specify a file name.')
         return None
