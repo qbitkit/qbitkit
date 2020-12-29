3541e7df674a368dbc7720acc9b92d7aef2e849c
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-28 17:18:39 -0800

    Customize Sphinx Config

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -10,7 +10,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
+from os.path import sep as pathsep
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
@@ -18,9 +18,8 @@
 # -- Project information -----------------------------------------------------
 
 project = 'qbitkit'
-copyright = '2020, Brian Lechthaler'
-author = 'Brian Lechthaler'
-
+author = f'{project} Team'
+copyright = f'2021, {author}'
 
 # -- General configuration ---------------------------------------------------
 
@@ -38,10 +37,34 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+source_suffix = ".rst"
+master_doc = "index"
+
+autoclass_content = "both"
+autodoc_member_order = "bysource"
+default_role = "py:obj"
+
+html_theme = "sphinx_rtd_theme"
+htmlhelp_basename = "{}doc".format(project)
+
+napoleon_use_rtype = False
+
+repo_root_path = f"..{pathsep}..{pathsep}"
+
+apidoc_module_dir = f"..{pathsep}..{pathsep}{project}"
+apidoc_output_dir = "_apidoc"
+apidoc_excluded_paths = [f"..{pathsep}..{pathsep}"]
+apidoc_separate_modules = True
+apidoc_module_first = True
+apidoc_extra_args = ["-f", "--implicit-namespaces", "-H", "API Reference"]
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build',
+                    'Thumbs.db',
+                    '.DS_Store',
+                    '__MACOSX']
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -49,7 +72,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+#html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
