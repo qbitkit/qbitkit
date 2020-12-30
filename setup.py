04809d28566c93e1b1ba0f606ca5706ad4a994b1
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-29 20:18:47 -0800

    Set project URL to future URL

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     description="Easy Quantum Computing Framework",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/brianlechthaler/qbitkit",
+    url="https://github.com/qbitkit/qbitkit",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
