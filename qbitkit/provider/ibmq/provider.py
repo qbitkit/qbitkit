92ab5efc73f4314dc6215f860556db7649f5c1da
Brian Lechthaler <brianlechthaler@protonmail.ch>
2020-12-31 14:49:30 -0800

    Correct additional notes in warning

--- qbitkit/provider/ibmq/provider.py
+++ qbitkit/provider/ibmq/provider.py
@@ -8,9 +8,10 @@ from qbitkit.error import error as qbitkit_error
 
 def get_support_status(self):
     ibmq_support_status = 'experimental'
+    resource_name = 'IBM Quantum Experience'
     qbitkit_error.errors.experimental_feature(feature_state=ibmq_support_status,
-                                              resource_name='IBM Quantum Experience',
-                                              additional_notes='For more information on forthcoming Elasticsearch support, see https://github.com/brianlechthaler/qbitkit/issues/4')
+                                              resource_name=resource_name,
+                                              additional_notes=f'For more information on forthcoming {resource_name} support, see https://github.com/brianlechthaler/qbitkit/issues/2')
     return ibmq_support_status
 
 get_support_status()
