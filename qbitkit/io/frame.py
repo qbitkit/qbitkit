3e87bb52388f15c641c68310f3c6682d1a77bc2b
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-02 13:13:56 -0800

    Documentation: Make Keyword Args Bulletpointed Lists

--- qbitkit/io/frame.py
+++ qbitkit/io/frame.py
@@ -5,7 +5,7 @@ class frame:
         """Return a pandas DataFrame optionally populated with specified data from keyword arguments
 
         Keyword arguments:
-        data -- specify data to populate the Pandas DataFrame with. (default None)"""
+        - data -- specify data to populate the Pandas DataFrame with. (default None)"""
         df = pd.DataFrame(data=data)
         return df
     def save_frame(frame=get_frame(),
@@ -13,9 +13,9 @@ class frame:
                    file_type='csv'):
         """Save a specified Pandas DataFrame as a specified file type at a specified location.
 
-        frame -- specify the dataframe that we will be saving to disk. (default get_frame())
-        pth -- specify the path where you want to save the dataframe. (default 'data.csv')
-        file_type -- what structure (type of file) to use when saving to disk. Can be one of 'csv', 'json', or 'excel'. (default 'csv')"""
+        - frame -- specify the dataframe that we will be saving to disk. (default get_frame())
+        - pth -- specify the path where you want to save the dataframe. (default 'data.csv')
+        - file_type -- what structure (type of file) to use when saving to disk. Can be one of 'csv', 'json', or 'excel'. (default 'csv')"""
         if file_type == 'csv':
             f.excel.write(data_path=pth,
                         df=frame)
