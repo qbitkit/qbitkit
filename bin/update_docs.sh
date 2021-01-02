bf4630f68989877a6f667b2cef6ffb0c784bf751
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-02 14:35:35 -0800

    Create update_docs.sh

---
+++ bin/update_docs.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+echo "Usage: ./update_docs.sh <make subcommand>"
+git pull
+. ../doc/sphinx/venv/bin/activate
+pip install -Ur ../requirements.txt
+pip install -Ur ../doc/sphinx/requirements.txt
+cd ..
+python setup.py build
+python setup.py install
+cd doc/sphinx/
+rm -rf _*
+make -j $(nproc) $1
