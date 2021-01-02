62c0ab49bdecd4f213fdb130294ee09019033d30
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-02 14:42:57 -0800

    Add Script for copying documentation to /var/www/html
    This script just copies the documentation to the right directory then restarts whatever webserver the user specifies

---
+++ bin/copy_docs.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo "Usage: ./copy_docs.sh <input directory> <output directory> <webserver>"
+export input=$1
+export output=$2
+rm -rf $output/*
+cp -r $input/* $output
+sudo service $webserver restart
