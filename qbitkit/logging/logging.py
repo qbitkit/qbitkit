836066f87390f64f9664021c9dbbffa4b1b603f2
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-03 11:30:49 -0800

    Put result-to-doc function inside gate_model class
    This is to differentiate from annealing support that will be added in the future. Gate model and Annealing quantum may have different results to process, so differentiating may be necessary as shown in this commit.

--- qbitkit/logging/logging.py
+++ qbitkit/logging/logging.py
@@ -1,21 +1,22 @@
 from qbitkit.logging import elasticsearch as es
 from datetime import datetime as dt
 class braket:
-    def doc(job=None):
-        """Create a dictionary object containing information from the results of a Braket task, then return it.
+    class gate_model:
+        def doc(job=None):
+            """Create a dictionary object containing information from the results of a Braket task, then return it.
 
-        Args:
-            job (braket.tasks.quantum_task.QuantumTask): a job object returned from running a Braket task.
-        Returns:
-            dict: returns a dict containing measurement probabilities, measurement counts, measured qubits, and task metadata."""
-        meas_probabilities = job.result().measurement_probabilities
-        meas_counts = job.result().measurement_counts
-        meas_qubits = job.result().measured_qubits
-        metadata = job.result().task_metadata
-        doc = {
-            'measurement_probabilities': meas_probabilities,
-            'measurement_counts': meas_counts,
-            'measured_qubits': meas_qubits,
-            'metadata': metadata,
-        }
-        return doc
+            Args:
+                job (braket.tasks.quantum_task.QuantumTask): a job object returned from running a Braket task.
+            Returns:
+                dict: returns a dict containing measurement probabilities, measurement counts, measured qubits, and task metadata."""
+            meas_probabilities = job.result().measurement_probabilities
+            meas_counts = job.result().measurement_counts
+            meas_qubits = job.result().measured_qubits
+            metadata = job.result().task_metadata
+            doc = {
+                'measurement_probabilities': meas_probabilities,
+                'measurement_counts': meas_counts,
+                'measured_qubits': meas_qubits,
+                'metadata': metadata,
+            }
+            return doc
