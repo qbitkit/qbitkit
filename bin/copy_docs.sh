b60a519d0f3a6f17364324f52dfe705511570aac
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-02 18:52:11 -0800

    Fix copy_docs.sh
    This fix uses

--- bin/copy_docs.sh
+++ bin/copy_docs.sh
@@ -2,6 +2,7 @@
 echo "Usage: ./copy_docs.sh <input directory> <output directory> <webserver>"
 export input=$1
 export output=$2
+export webserver=$3
 rm -rf $output/*
 cp -r $input/* $output
 sudo service $webserver restart
