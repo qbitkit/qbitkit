233283277e4164b4d6eb5cdf6b012c19eb942127
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-02 17:49:09 -0800

    Documentation: Reformat to Google-Style Docstrings

--- qbitkit/provider/ibmq/provider.py
+++ qbitkit/provider/ibmq/provider.py
@@ -19,23 +19,31 @@ get_support_status()
 # Loading your IBM Q account(s)
 class connection:
     def get_provider(hub='ibm-q'):
-        """Create an IBMQ provider with Qiskit to be used as a device in qbitkit"""
+        """Create an IBMQ provider with Qiskit to be used as a device in qbitkit
+        Returns:
+            qiskit.providers.BaseProvider: the IBMQ provider"""
         provider = IBMQ.get_account(hub)
         return provider
 class local:
     def aer(simulator='qasm_simulator'):
-        """Create an Aer simulator from Qiskit."""
+        """Create an Aer simulator from Qiskit.
+        Returns:
+            qiskit.providers.BaseBackend: the Aer simulator from Qiskit"""
         a.get_backend(simulator)
     def basic_aer(simulator='qasm_simulator'):
-        """Create a Basic Aer simulator from Qiskit."""
+        """Create a Basic Aer simulator from Qiskit.
+        Returns:
+            qiskit.providers.BaseBackend: the BasicAer simulator from Qiskit"""
         ba.get_backend(simulator)
     def sim(simulator='qasm_simulator',
             backend='basic_aer'):
         """Create an IBMQ Simulator.
 
-        Keyword arguments:
-        - simulator -- the type of simulator to use (default 'qasm_simulator')
-        - backend -- the backend to use. can be 'aer' or 'basic_aer'. (default 'basic_aer')"""
+        Args:
+            simulator (str): the type of simulator to use (default 'qasm_simulator')
+            backend (str): the backend to use. can be 'aer' or 'basic_aer'. (default 'basic_aer')
+        Returns:
+            qiskit.providers.BaseBackend: the simulator you chose"""
         if backend == 'basic_aer':
             device = local.basic_aer(simulator=simulator)
             return device
@@ -52,10 +60,10 @@ class job:
               shots=8192):
         """Create a job with the least busy provider capable of fulfilling our request, then return the job object.
 
-        Keyword arguments:
-        - circuit -- the circuit to run on the QPU. (default None)
-        - backend -- the backend to use. (default None)
-        - shots -- the number of shots or 'reads' to take when executing on QPU. (default 8192)"""
+        Args:
+            circuit (qiskit.providers.ibmq.job.IBMQ): the circuit to run on the QPU. (default None)
+            backend (qiskit.providers.ibmq.IBMQBackend): the backend to use. (default None)
+            shots (int): the number of shots or 'reads' to take when executing on QPU. (default 8192)"""
         IBMQ.load_account()
         provider = IBMQ.get_provider(hub='ibm-q')
         backend = least_busy(provider.backends(filters=lambda b: b.configuration().n_qubits >= 3 and
@@ -69,10 +77,12 @@ class job:
                 show_graph=False):
         """Run a specified job and return the results from the QPU.
 
-        Keyword arguments:
-        - circuit -- the circuit to be run on the QPU. (default None)
-        - job -- the job object to run on the QPU. (default None)
-        - show_graph -- whether or not to show a graph of the measurement probabilities. (default False)"""
+        Args:
+            circuit (braket.circuits.Circuit): the circuit to be run on the QPU. (default None)
+            job (qiskit.providers.ibmq.job.IBMQJob): the job object to run on the QPU. (default None)
+            show_graph (bool): whether or not to show a graph of the measurement probabilities. (default False)
+        Returns:
+            dict: the results from running the job"""
         result = job.result()
         if show_graph == False:
             return result
