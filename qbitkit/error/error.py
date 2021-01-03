af750e6c8845c4a7eedfe5cd24f132b49e1e80e6
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-02 17:01:52 -0800

    Documentation: Reformat to Google-Style Docstrings

--- qbitkit/error/error.py
+++ qbitkit/error/error.py
@@ -12,9 +12,11 @@ class log:
             print_msg=False):
         """Attaches a timestamp to a specified message, and prints the message if specified. Returns formatted message.
 
-        Keyword arguments:
-        - msg -- the message you would like to prepend a timestamp to and return and optionally print. (default None)
-        - print_msg -- specify whether or not to print the message in addition to returning it as usual. (default False)"""
+        Args:
+            msg (str): the message you would like to prepend a timestamp to and return and optionally print. (default None)
+            print_msg (str): specify whether or not to print the message in addition to returning it as usual. (default False)
+        Returns:
+            str: timestamped and formatted message"""
         message = str(log.timestamp()) + msg
         if print_msg == True:
             print(str(log.timestamp()), msg)
@@ -24,9 +26,9 @@ class errors:
                       level='Warning'):
         """Throw an error warning the user that a specified resource has not been specified.
 
-        Keyword arguments:
-        - resource -- name of resource to use in error message. (default None)
-        - level -- string with desired warning level (default 'Warning')"""
+        Args:
+            resource (str): name of resource to use in error message. (default None)
+            level (str): string with desired warning level (default 'Warning')"""
         message = f'{level}: No {resource} has been specified.'
         log.msg(msg=message, print_msg=True)
         return message
@@ -35,10 +37,12 @@ class errors:
                        additional_notes=None):
         """Generate a warning based on specified keyword arguments that warns users that a certain feature is at a certain support stage.
 
-        Keyword arguments:
-        - feature_state -- The state of the feature, such as experimental or beta. (default 'experimental')
-        - resource_name -- The name of the feature (resource) to display the feature state of. (default 'this qbitkit feature')
-        - additional_notes -- Tack on a note to the end of the warning (default None)"""
+        Args:
+            feature_state (str): The state of the feature, such as experimental or beta. (default 'experimental')
+            resource_name (str): The name of the feature (resource) to display the feature state of. (default 'this qbitkit feature')
+            additional_notes (str): Tack on a note to the end of the warning (default None)
+        Returns:
+            str: the generated error message based on specified keyword arguments."""
         message = f"Support for {resource_name} is still in the '{feature_state}' stage. Please use with caution. {additional_notes}"
         log.msg(msg=message,
                 print_msg=True)
