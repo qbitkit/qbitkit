fe62c4a6dcc62a7b7c46ded6ceab572d2f1f3853
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-06 23:11:31 -0800

    Add Function to Create a Bell State Circuit
    Last commit was just documentation due to me prematurely commiting changes.  Would this be considered documentation-driven development?

--- qbitkit/circuit/circuits/basic.py
+++ qbitkit/circuit/circuits/basic.py
@@ -1,7 +1,21 @@
-def bell(iterations=1,
-         append=None):
+from qbitkit.io import frame as f
+
+def bell(append=None):
     """Make a 2-qubit Bell State Circuit using 1 Hadamard gate and 1 Controlled NOT gate.
 
     Args:
-        iterations(int): number of times to repeat the circuit (default 1)
-        append(pandas.DataFrame): an existing Pandas DataFrame to append the Bell State Circuit to."""
+        append(pandas.DataFrame): an existing Pandas DataFrame to append the Bell State Circuit to.
+    Returns:
+        pandas.DataFrame: Pandas DataFrame containing the Bell State Circuit, or a DataFrame you specified with the Bell State Circuit appended to it."""
+    bellFrame = f.get_frame(data={'gate': ['h', 'cnot'],
+                                  'targetA': [0, 0],
+                                  'targetB': [None, 1],
+                                  'targetC': [None, None], })
+    if append == None:
+        return bellFrame
+    elif append != None:
+        bellFrameAppended = append.append(bellFrame)
+        return bellFrameAppended
+    else:
+        bellFrameAppended = append.append(bellFrame)
+        return bellFrameAppended
