34f20a7ac1eeb4fd0c8f1ff23e1c71821dca8c04
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-07 19:07:28 -0800

    Add GHZ State Circuit

--- qbitkit/circuit/circuits/basic.py
+++ qbitkit/circuit/circuits/basic.py
@@ -19,3 +19,22 @@ def bell(append=None):
     else:
         bellFrameAppended = append.append(bellFrame)
         return bellFrameAppended
+def ghz(append=None):
+    """Make a 2-qubit Greenberger-Horne-Zeilinger (GHZ) State Circuit using 1 Hadamard gate and 2 Controlled NOT gates.
+
+        Args:
+            append(pandas.DataFrame): an existing Pandas DataFrame to append the GHZ State Circuit to.
+        Returns:
+            pandas.DataFrame: Pandas DataFrame containing the GHZ State Circuit, or a DataFrame you specified with the GHZ State Circuit appended to it."""
+    ghzFrame = f.get_frame(data={'gate':['h','cnot','cnot'],
+                                 'targetA':[0,1,2],
+                                 'targetB':[None,1,2],
+                                 'targetC':[None,None,None]})
+    if append == None:
+        return bellFrame
+    elif append != None:
+        bellFrameAppended = append.append(bellFrame)
+        return bellFrameAppended
+    else:
+        bellFrameAppended = append.append(bellFrame)
+        return bellFrameAppended
