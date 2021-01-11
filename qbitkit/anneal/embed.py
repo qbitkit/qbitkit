e06c41d6b67e78567ea1b32a9cb781e1e3987352
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-11 15:32:37 -0800

    :pencil: Document qbitkit.anneal.embed.composite()

--- qbitkit/anneal/embed.py
+++ qbitkit/anneal/embed.py
@@ -1,4 +1,10 @@
 from dwave.system.composites import EmbeddingComposite as _ec
 def composite(sampler=None):
+    """Returns EmbeddingComposite based on specified sampler.
+
+    Args:
+        sampler (dwave.system.samplers): Any Sampler compatible with the D-Wave Ocean SDK. (default None)
+    Returns:
+        dwave.system.composites.EmbeddingComposite: An EmbeddingComposite based on the specified sampler."""
     EmbeddingCompositeSampler = _ec(sampler)
     return EmbeddingCompositeSampler
