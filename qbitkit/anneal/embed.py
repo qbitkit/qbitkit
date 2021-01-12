922b681f910f12bf8ed5a94999136f24725c51ac
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-11 17:40:48 -0800

    :new: Add function to generate embeddings from any QUBO

--- qbitkit/anneal/embed.py
+++ qbitkit/anneal/embed.py
@@ -1,4 +1,7 @@
 from dwave.system.composites import EmbeddingComposite as _ec
+import minorminer as _mm
+from dwave.embedding import embed_qubo
+
 def composite(sampler=None):
     """Returns EmbeddingComposite based on specified sampler.
 
@@ -8,3 +11,12 @@ def composite(sampler=None):
         dwave.system.composites.EmbeddingComposite: An EmbeddingComposite based on the specified sampler."""
     EmbeddingCompositeSampler = _ec(sampler)
     return EmbeddingCompositeSampler
+def qubo(sampler=None,
+         qubo=None):
+    _, target_edgelist, target_adjacency = sampler.structure
+    embedding = _mm.find_embedding(qubo,
+                                          target_edgelist)
+    qubo_embedded = embed_qubo(qubo,
+                               embedding,
+                               target_adjacency)
+    return qubo_embedded
