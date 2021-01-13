cd4ab0597990123162e6b1fb05993cd2796bf937
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-12 16:18:11 -0800

    :new: Add a function to convert CSP to QUBO

--- qbitkit/anneal/bcsp.py
+++ qbitkit/anneal/bcsp.py
@@ -2,3 +2,7 @@ import dwavebinarycsp as _dbc
 def new(vartype='BINARY'):
   dbc = _dbc.ConstraintSatisfactionProblem(vartype)
   return dbc
+class convert:
+    def to_qubo(self):
+        qubo = self.to_qubo()
+        return qubo
