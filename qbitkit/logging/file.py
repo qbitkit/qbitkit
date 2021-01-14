0511fdcc7f5570c487af98d16bca068e6d5f35f3
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-13 19:27:23 -0800

    :new: Add Function to Log to a Local File

---
+++ qbitkit/logging/file.py
@@ -0,0 +1,9 @@
+from qbitkit.io import frame as __fr
+
+def save(frame=__fr.frame.get_frame(),
+         file_type='csv',
+         pth=f'output{}'):
+    __fr.frame.save_frame(frame=frame,
+                          pth=pth,
+                          file_type=file_type)
+    return f"{pth}.{file_type}"
