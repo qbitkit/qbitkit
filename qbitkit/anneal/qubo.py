d7a09336c81c96099d40316fe98de9538ce2683d
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-13 19:50:22 -0800

    :new: :pencil: Doc: Document Function qubo.from_bqm()

--- qbitkit/anneal/qubo.py
+++ qbitkit/anneal/qubo.py
@@ -1,3 +1,9 @@
 def from_bqm(bqm=None):
+    """Convert a given BQM into a QUBO.
+
+    Args:
+        bqm(dimod.binary_quadratic_model.BinaryQuadraticModel): A BQM to convert into a QUBO.
+    Returns:
+        tuple: A tuple containing the QUBO converted from the specified BQM."""
     qubo, offset = bqm.to_qubo()
     return qubo, offset
