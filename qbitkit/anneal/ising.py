b0e02f81239f7dd6769cb7ae4e715cf4af37b901
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-14 15:54:13 -0800

    :pencil: :wrench: Doc Finetune: Specify Default Arguments

--- qbitkit/anneal/ising.py
+++ qbitkit/anneal/ising.py
@@ -2,7 +2,7 @@ def from_bqm(bqm=None):
     """Convert a given BQM into an Ising.
 
     Args:
-        bqm(dimod.binary_quadratic_model.BinaryQuadraticModel): A BQM to convert into an Ising.
+        bqm(dimod.binary_quadratic_model.BinaryQuadraticModel): A BQM to convert into an Ising. (default None)
     Returns:
         tuple: A tuple containing the Ising converted from the specified BQM."""
     ising = bqm.to_ising()
