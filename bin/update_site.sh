361c03cee0d3e149a38310f0e5285637d7eb1f90
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-14 18:25:16 -0800

    :new: Add a Script to build+update docs & site

---
+++ bin/update_site.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+echo "Usage: ./update_site.sh <make subcommand> <path to copy site to>"
+echo "Updating APT..."
+sudo apt-get update > /dev/null
+echo "Upgrading APT..."
+sudo apt-get upgrade -y -qq > /dev/null
+echo "Pulling latest updates from git..."
+git pull
+. ../doc/sphinx/venv/bin/activate
+echo "Ensuring dependencies are up-to-date..."
+pip install -Ur ../requirements.txt > /dev/null
+pip install -Ur ../doc/sphinx/requirements.txt > /dev/null
+cd ..
+echo "Re-building qbitkit..."
+python setup.py build > /dev/null
+echo "Re-installing qbitkit..."
+python setup.py install > /dev/null
+cd doc/sphinx/
+rm -rf _*
+echo "Compiling documentation..."
+make -j $(nproc) $1
+echo "Injecting homepage..."
+mkdir -p homepage/docs
+mv _build/html/* homepage/docs
+rm -rf _build/html/*
+mv homepage/* _build/html/
+echo "Copying compiled docs to specified directory..."
+sudo /bin/sh ../../bin/copy_docs.sh _build/html $2 nginx
