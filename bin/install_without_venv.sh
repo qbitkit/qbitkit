5be45ff49880b04711f039f940e96414bddd1ae5
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-16 14:10:15 -0800

    Revert ":wrench: Fine Tuning: User Install, No Root."
    This reverts commit 3c3aeb7477e1fa47228a2dd483ac855c491da7db.

--- bin/install_without_venv.sh
+++ bin/install_without_venv.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 # Update Pip
-python3 -m pip install --user -U pip
+sudo -H python3 -m pip install -U pip
 # Go to the parent directory where the root of the repo should be
 cd ..
 # Make sure pip and setuptools inside the virtualenv are up-to-date
-python3 -m pip install --user -U pip
+sudo -H python3 -m pip install -U pip
 # Install Dependencies
-python3 -m pip install --user -U -r requirements.txt
+sudo -H python3 -m pip install -U -r requirements.txt
 # Build python package
 python3 setup.py build
 # Install Python package
