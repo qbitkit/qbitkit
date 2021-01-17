9f0a3e2e1a036bc0c528c645d54f2736e7c3679a
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-17 13:49:23 -0800

    :wrench: Fine Tuning: Rename Function and Put it in new class

--- qbitkit/provider/ibmq/circuit/scale.py
+++ qbitkit/provider/ibmq/circuit/scale.py
@@ -1,8 +1,9 @@
-def circuit(circuit=None):
-    """Appends a circuit to itself.
-    Args:
-        circuit(braket.circuits.Circuit): a Qiskit circuit to append to itself. (default None)
-    Returns:
-        qiskit.QuantumCircuit: a scaled-up Qiskit QuantumCircuit."""
-    circuit = circuit.append(circuit)
-    return circuit
+class Circuit:
+    def append(circuit=None):
+        """Appends a circuit to itself.
+        Args:
+            circuit(braket.circuits.Circuit): a Qiskit circuit to append to itself. (default None)
+        Returns:
+            qiskit.QuantumCircuit: a scaled-up Qiskit QuantumCircuit."""
+        circuit = circuit.append(circuit)
+        return circuit
