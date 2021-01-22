5b32915dc53b9a7d479212ee1036fdb8fc893139
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-21 19:34:31 -0800

    :new: :pencil: Document new Function qbitkit.io.list.remove_value()

--- qbitkit/io/list.py
+++ qbitkit/io/list.py
@@ -1,4 +1,11 @@
 def remove_value(self=list([]), value=None):
+    """Removes a specified value from a specified list, and returns a list without the specified value included.
+
+    Args:
+        self(list): A list to remove values from. (default list([]))
+        value(*): Value to remove from list. Can be any value. (default None)
+    Returns:
+        list: List with specified values removed."""
     cleaned_list = list([])
     for item in list:
         if item is not None:
