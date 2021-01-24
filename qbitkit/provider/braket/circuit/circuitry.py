7f30c98e1019a8d972f043500fb4c07790cf07b8
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-23 22:15:40 -0800

    :new: :pencil: Add in-line doc to describe each translatable gate in translate_gate()

--- qbitkit/provider/braket/circuit/circuitry.py
+++ qbitkit/provider/braket/circuit/circuitry.py
@@ -42,129 +42,161 @@ class Translate:
             unitary_targets (list): a list of targets for the unitary. (default [0])
         Returns:
             braket.circuits.Circuit: Quantum circuit for Braket with gate we translated appended to it"""
+        # Unitary Gate
         if op == 'unitary':
             input_circuit = input_circuit.unitary(matrix=unitary_matrix,
                                                   targets=unitary_targets)
             return input_circuit
+        # Hadamard Gate
         if op == 'h':
             input_circuit = input_circuit.h(targetA)
             return input_circuit
+        # X Gate
         if op == 'x':
             input_circuit = input_circuit.x(targetA)
             return input_circuit
+        # Y Gate
         if op == 'y':
             input_circuit = input_circuit.y(targetA)
             return input_circuit
+        # Z Gate
         if op == 'z':
             input_circuit = input_circuit.z(targetA)
             return input_circuit
+        # S Gate
         if op == 's':
             input_circuit = input_circuit.s(targetA)
             return input_circuit
+        # T Gate
         if op == 't':
             input_circuit = input_circuit.t(targetA)
             return input_circuit
+        # Square Root Gate
         if op == 'v':
             input_circuit = input_circuit.v(targetA)
             return input_circuit
+        # Square Root (inverted)
         if op == 'vi':
             input_circuit = input_circuit.vi(targetA)
             return input_circuit
+        # S (inverted)
         if op == 'si':
             input_circuit = input_circuit.si(targetA)
             return input_circuit
+        # T (inverted)
         if op == 'ti':
             input_circuit = input_circuit.ti(targetA)
             return input_circuit
+        # XX Gate
         if op == 'xx':
             input_circuit = input_circuit.xx(targetA,
                                              targetB,
                                              theta)
             return input_circuit
+        # XY Gate
         if op == 'xy':
             input_circuit = input_circuit.xx(targetA,
                                              targetB,
                                              theta)
             return input_circuit
+        # YY Gate
         if op == 'yy':
             input_circuit = input_circuit.yy(targetA,
                                              targetB,
                                              theta)
             return input_circuit
+        # ZZ Gate
         if op == 'zz':
             input_circuit = input_circuit.zz(targetA,
                                              targetB,
                                              theta)
             return input_circuit
+        # Identity SWAP Gate
         if op == 'iswap':
             input_circuit = input_circuit.iswap(targetA,
                                                 targetB)
             return input_circuit
+        # Phase Shift Gate
         if op == 'phaseshift':
             input_circuit = input_circuit.phaseshift(targetA,
                                                      phi)
             return input_circuit
+        # Controlled Y
         if op == 'cy':
             input_circuit = input_circuit.cy(targetA,
                                              targetB)
             return input_circuit
+        # Controlled Z
         if op == 'cz':
             input_circuit = input_circuit.cz(targetA,
                                              targetB)
             return input_circuit
+        # Identity
         if op == 'i':
             input_circuit = input_circuit.i(targetA)
             return input_circuit
+        # Rotate around X axis
         if op == 'rx':
             input_circuit = input_circuit.rx(targetA,
                                              angle)
             return input_circuit
+        # Rotate around Y axis
         if op == 'ry':
             input_circuit = input_circuit.ry(targetA,
                                              angle)
             return input_circuit
+        # Rotate around Z axis
         if op == 'rz':
             input_circuit = input_circuit.rz(targetA,
                                              angle)
             return input_circuit
+        # SWAP Gate
         if op == 'swap':
             input_circuit = input_circuit.swap(targetA,
                                                targetB)
             return input_circuit
+        # Controlled NOT
         if op == 'cnot':
             input_circuit = input_circuit.cnot(targetA,
                                                targetB)
             return input_circuit
+        # Double Controlled NOT (aka Toffoli Gate)
         if op == 'ccnot':
             input_circuit = input_circuit.ccnot(targetA,
                                                 targetB,
                                                 targetC)
             return input_circuit
+        # Controlled Phase Shift Gate
         if op == 'cphaseshift':
             input_circuit = input_circuit.cphaseshift(targetA,
                                                       targetB,
                                                       angle)
             return input_circuit
+        # Controlled Phase Shift Gate that phases the |00> state
         if op == 'cphaseshift00':
             input_circuit = input_circuit.cphaseshift00(targetA,
                                                         targetB,
                                                         angle)
             return input_circuit
+        # Controlled Phase Shift Gate that phases the |01> state
         if op == 'cphaseshift01':
             input_circuit = input_circuit.cphaseshift01(targetA,
                                                         targetB,
                                                         angle)
             return input_circuit
+        # Controlled Phase Shift Gate that phases the |10> state
         if op == 'cphaseshift10':
             input_circuit = input_circuit.cphaseshift10(targetA,
                                                         targetB,
                                                         angle)
             return input_circuit
+        # Controlled SWAP (aka Fredkin Gate)
         if op == 'cswap':
             input_circuit = input_circuit.cswap(targetA,
                                                 targetB,
                                                 targetC)
             return input_circuit
+        # Parametrized SWAP Gate
         if op == 'pswap':
             input_circuit = input_circuit.pswap(targetA,
                                                 targetB,
