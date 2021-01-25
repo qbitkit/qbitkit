42c532ba4f71a5b174d630b5ee331b3ccb4706cb
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-24 16:24:10 -0800

    :new: :pencil: Add inline documentation to anneal.embed()

--- qbitkit/anneal/embed.py
+++ qbitkit/anneal/embed.py
@@ -11,7 +11,9 @@ def composite(sampler=None):
         sampler (dwave.system.samplers): Any Sampler compatible with the D-Wave Ocean SDK. (default None)
     Returns:
         dwave.system.composites.EmbeddingComposite: An EmbeddingComposite based on the specified sampler."""
+    # Create an EmbeddingComposite from the specified sampler.
     EmbeddingCompositeSampler = _ec(sampler)
+    # Return the embedded sampler.
     return EmbeddingCompositeSampler
 
 
@@ -24,12 +26,16 @@ def qubo(sampler=None,
         qubo(tuple): A tuple containing the QUBO to map onto the given sampler's QPU topology. (default None)
     Returns:
         dict: The given QUBO mapped to the given sampler's QPU topology."""
+    # Get the topology data of the D-Wave QPU for the given sampler.
     _, target_edgelist, target_adjacency = sampler.structure
+    # Find the embedding based on the given QUBO and the target edges from the topology data..
     embedding = _mm.find_embedding(qubo,
                                    target_edgelist)
+    # Create embedding from the QUBO.
     qubo_embedded = embed_qubo(qubo,
                                embedding,
                                target_adjacency)
+    # Return the generated embedding.
     return qubo_embedded
 
 
@@ -42,12 +48,16 @@ def ising(sampler=None,
         ising(tuple): A tuple containing the Ising to map onto the given sampler's QPU topology. (default None)
     Returns:
         dict: The given Ising mapped to the given sampler's QPU topology."""
+    # Get the topology data of the D-Wave QPU from the given sampler.
     _, target_edgelist, target_adjacency = sampler.structure
+    # Find the embedding based on the given Ising and target edges from the topology data.
     embedding = _mm.find_embedding(ising,
                                    target_edgelist)
+    # Create embedding from the Ising.
     ising_embedded = __embed_ising__(ising,
                                      embedding,
                                      target_adjacency)
+    # Return the generated embedding.
     return ising_embedded
 
 
@@ -60,10 +70,14 @@ def bqm(sampler=None,
         bqm(dimod.binary_quadratic_model.BinaryQuadraticModel): A tuple containing the BQM to map onto the given sampler's QPU topology. (default None)
     Returns:
         dict: The given BQM mapped to the given sampler's QPU topology."""
+    # Get the topology data of the D-Wave QPU from the given sampler.
     _, target_edgelist, target_adjacency = sampler.structure
+    # Find the embedding based on the given BQM and target edges from the topology data.
     embedding = _mm.find_embedding(bqm,
                                    target_edgelist)
+    # Create embedding from the BQM.
     bqm_embedded = __embed_ising__(bqm,
                                    embedding,
                                    target_adjacency)
+    # Return the generated embedding.
     return bqm_embedded
