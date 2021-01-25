77ec41b4d76449ee815bbb15f3da299dbdbaac44
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-24 16:17:19 -0800

    :new: :pencil: Add Inline Documentation to anneal.ising()

--- qbitkit/anneal/ising.py
+++ qbitkit/anneal/ising.py
@@ -8,7 +8,9 @@ def from_bqm(bqm=None):
         bqm(dimod.binary_quadratic_model.BinaryQuadraticModel): A BQM to convert into an Ising. (default None)
     Returns:
         tuple: A tuple containing the Ising converted from the specified BQM."""
+    # Convert the BQM to an Ising
     ising = bqm.to_ising()
+    # Return Ising
     return ising
 
 
@@ -26,12 +28,18 @@ class Solve:
             auto_embed(bool): Whether or not to automatically embed the Ising to the given sampler. (default True)
         Returns:
             dict: a dictionary containing the results from the sampler."""
+        # Check if auto-embedding is enabled.
         if auto_embed is True:
+            # Embed the Ising on to the D-Wave QPU's topology.
             embedded_sampler = __embed__.ising(sampler,
                                                self)
+            # Sample using the embedded sampler.
             result = embedded_sampler.sample_ising(self,
                                                    num_reads=shots)
+        # Check if auto-embedding is disabled.
         elif auto_embed is False:
+            # Sample the ising equation with the given sampler.
             result = sampler.sample_ising(self,
                                           num_reads=shots)
+        # Return the result of sampling the Ising.
         return result
