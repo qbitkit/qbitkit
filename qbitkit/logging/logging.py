6fde4ba5063ced4e1eab77a435eee2a748b18bfc
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-25 22:44:38 -0800

    :pencil: Add Inline Doc to logging.logging()

--- qbitkit/logging/logging.py
+++ qbitkit/logging/logging.py
@@ -7,12 +7,17 @@ class Braket:
                 job (braket.tasks.quantum_task.QuantumTask): a job object returned from running a Braket task. (default None)
             Returns:
                 dict: returns a dict containing measurement probabilities, measurement counts, measured qubits, and task metadata."""
+            # Extract Counts of Each Bit Measured.
             meas_counts = job.result().measurement_counts
+            # Extract Measured Qubits.
             meas_qubits = job.result().measured_qubits
+            # Extract Task Metadata.
             metadata = job.metadata()
+            # Create Elasticsearch document using the 3 things we just collected.
             doc = {
                 'measurement_counts': meas_counts,
                 'measured_qubits': meas_qubits,
                 'metadata': metadata,
             }
+            # Return the generated Elasticsearch document.
             return doc
