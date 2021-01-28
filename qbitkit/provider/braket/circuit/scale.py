02ed8b0e039df0667687bfb1e3cdf36a768756c6
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-01-27 21:59:54 -0800

    :pencil: Add In-line documentatino to braket.circuit.scale()

--- qbitkit/provider/braket/circuit/scale.py
+++ qbitkit/provider/braket/circuit/scale.py
@@ -5,5 +5,7 @@ def circuit(circuit=None):
         circuit(braket.circuits.Circuit): a Braket circuit to append to itself. (default None)
     Returns:
         braket.circuits.Circuit: a scaled-up Braket circuit."""
+    # Append the circuit to itself.
     circuit = circuit.append(circuit)
+    # Return the circuit appended to itself.
     return circuit
