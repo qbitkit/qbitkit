bb468e0105b70c88567e4266a44b524b11c9634c
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-15 22:26:56 -0800

    :new: Implement Unit Testing for Qiskit BasicAer Simulators

--- tests/test_ibmq.py
+++ tests/test_ibmq.py
@@ -1,6 +1,7 @@
 import unittest as __ut__
 from qbitkit.provider.ibmq import provider as __p__
 from qiskit import Aer as __a__
+from qiskit import BasicAer as __ba__
 
 
 class TestAerSimulators(__ut__.TestCase):
@@ -9,6 +10,11 @@ class TestAerSimulators(__ut__.TestCase):
         qbitkit_aer_type = type(__p__.Local.aer())
         self.assertEqual(qiskit_aer_type,
                          qbitkit_aer_type)
+    def test_basicaer_creation(self):
+        qiskit_basicaer_type = type(__ba__.get_backend('qasm_simulator'))
+        qbitkit_basicaer_type = type(__p__.Local.basic_aer())
+        self.assertEqual(qiskit_basicaer_type,
+                         qbitkit_basicaer_type)
 
 
 if __name__ == '__main__':
