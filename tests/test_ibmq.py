f130571cdbcddacb95fdc4b837dd01c31f1c85e6
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-15 22:29:22 -0800

    :new: Add Unit Test for Alternate Method of Getting Simulators

--- tests/test_ibmq.py
+++ tests/test_ibmq.py
@@ -15,6 +15,16 @@ class TestAerSimulators(__ut__.TestCase):
         qbitkit_basicaer_type = type(__p__.Local.basic_aer())
         self.assertEqual(qiskit_basicaer_type,
                          qbitkit_basicaer_type)
+    def test_alt_aer_creation(self):
+        qiskit_aer_type = type(__a__.get_backend('qasm_simulator'))
+        qbitkit_aer_type = type(__p__.Local.sim(backend='aer'))
+        self.assertEqual(qiskit_aer_type,
+                         qbitkit_aer_type)
+    def test_alt_basicaer_creation(self):
+        qiskit_basicaer_type = type(__ba__.get_backend('qasm_simulator'))
+        qbitkit_basicaer_type = type(__p__.Local.sim(backend='basic_aer'))
+        self.assertEqual(qiskit_basicaer_type,
+                         qbitkit_basicaer_type)
 
 
 if __name__ == '__main__':
