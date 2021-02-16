2e4ed65306f63c9a6141bbdbeb97f6864d501235
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-15 16:26:28 -0800

    :new: Add Unit Test for Measurements

--- tests/qasm.py
+++ tests/qasm.py
@@ -2,6 +2,7 @@
 import unittest as __ut__
 from os import linesep as __sep__
 from qbitkit.qasm import generate as __g__
+from numpy.random import randint as __rand__
 
 
 # TestCase for QASM Generation (qbitkit.qasm.generate)
@@ -21,3 +22,22 @@ class TestGeneration(__ut__.TestCase):
                   f"--> Here's what we got: {__sep__}'{headers}'")
             correct_headers = False
         self.assertEqual(correct_headers, True)
+    def measurements(self):
+        # Initialize variables
+        test_q = __rand__(0, 1024)
+        test_c = __rand__(0, 1024)
+        actual_expected_match = False
+        # Set expeced value
+        expected_measurements = f"measure q[{test_q}] -> c[{test_c}];"
+        # Get actual value
+        actual_measurements = __g__.measurement(test_q,
+                                                test_c)
+        # Check if actual and expected values match as they should
+        if actual_measurements == expected_measurements:
+            actual_expected_match = True
+        else:
+            print("--> Actual and Expected Values Differ.",
+                  f"---> Expected Value: '{str(expected_measurements)}'",
+                  f"---> Actual Value: '{str(actual_measurements)}'")
+            actual_expected_match = False
+        self.assertEqual(actual_expected_match, True)
