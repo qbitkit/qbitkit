06102bf641d408e8bcadf3ae0622153d8521a96b
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-17 14:57:19 -0800

    :wrench: Split State Prep into its own Function

--- tests/test_braket.py
+++ tests/test_braket.py
@@ -4,7 +4,8 @@ from qbitkit.provider.braket import provider as __p__
 from braket.circuits import Circuit as __c__
 
 class TestOfflineSimulator(__tc__):
-    def test_local_sim(self):
+    def state_prep(self=None):
+        # Create New Circuit
         test_circ = __c__()
         # Set All Qubits to |1>
         for i in range(16):
@@ -16,6 +17,9 @@ class TestOfflineSimulator(__tc__):
         test_circ = test_circ.x(1).x(2).x(3).x(7)
         # Second Byte 01101011 k
         test_circ = test_circ.x(9).x(10).x(12).x(14).x(15)
+        return test_circ
+    def test_local_sim(self):
+        test_circ = TestOfflineSimulator.state_prep()
         # Create Local Simulator
         lsim = __p__.Local.sim()
         ls = lsim('default')
