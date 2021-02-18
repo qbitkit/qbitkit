3261f857d084453534bf895951b754b829334b34
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-18 14:35:28 -0800

    :new: Add Function io.list.count_range()

--- qbitkit/io/list.py
+++ qbitkit/io/list.py
@@ -14,3 +14,22 @@ def fill_range(fill=str('m'),
     for iteration in range(iterations):
         append.append(fill)
     return append
+
+def count_range(start=int(0),
+                end=int(9),
+                append=None):
+    """Count between values start and end, appending each value to a list.
+
+    Args:
+        start(int): Number to start counting at. (default int(0))
+        end(int): Number to stop counting at. (default int(9))
+        append(list): List to append generated list to, if set to None function will not append to anything. (default None)
+    Returns:
+        list: Generated list.
+    """
+    specified_range = range(int(start),
+                            int(end))
+    generated_list = list([])
+    for iteration in specified_range:
+        generated_list.append(int(iteration))
+    return generated_list
