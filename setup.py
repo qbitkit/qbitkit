bba41a8c6e806ca7676470d07c3e882581a284bc
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-17 21:25:35 -0800

    :new: Use New setup.py from branch main

--- setup.py
+++ setup.py
@@ -1,5 +1,25 @@
 from setuptools import find_packages as __findpkg__
 from setuptools import setup as __setup__
+from subprocess import run as __run__
+from os import linesep as __ls__
+
+
+def __cmd_runner__(cmd=None):
+    cmd_result = __run__(cmd,
+                         capture_output=True)
+    stdout_raw = cmd_result.stdout
+    return stdout_raw
+def __process_stdout__(stdout=None):
+    stdout_str = str(stdout.decode('utf-8'))
+    stdout_fix = stdout_str.strip('\n')
+    return stdout_fix
+def __get_branch_name__(self=None):
+    git_cmd = ["git", "rev-parse",
+               "--abbrev-ref", "HEAD"]
+    stdout_raw = __cmd_runner__(git_cmd)
+    stdout_fixed = __process_stdout__(stdout_raw)
+    return stdout_fixed
+
 
 
 with open("README.md", "r", encoding="utf-8") as fh:
@@ -7,7 +27,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 __setup__(
     name="qbitkit",
-    version="0.1.0",
+    version=__get_branch_name__(),
     author="qbitkit Team",
     author_email="brianlechthaler@protonmail.ch",
     description="Quantum Computing, for Humans.",
