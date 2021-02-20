a418016db1228dfc13c4dcf7ed94f2b66c525c45
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-02-19 18:57:26 -0800

    :new: Add Function compare to class QKTestCase

--- tests/qktest.py
+++ tests/qktest.py
@@ -1,5 +1,6 @@
 from qbitkit.io import frame as __f__
 from unittest import TestCase as __tc__
+from os import linesep as __sep__
 
 
 class QKTestCase(__tc__):
@@ -35,3 +36,17 @@ class QKTestCase(__tc__):
             frame = xgates_frame.append(measurements_frame)
 
         return frame
+    def compare(actual_value=None,
+                expected_value=None):
+        expected_equals_actual = False
+        if actual_value == expected_value:
+            print(f"--> Success! {__sep__}",
+                  f"---> Expected value: '{expected_value}'",
+                  f"= Actual Value: {actual_value}")
+            expected_equals_actual = True
+        else:
+            print("--> Actual Output Did not Match Expected Output.",
+                  f"{__sep__}---> Expected Output: '{expected_value}'",
+                  f"{__sep__}---> Actual Value: '{actual_value}'")
+            expected_equals_actual = False
+        return expected_equals_actual
