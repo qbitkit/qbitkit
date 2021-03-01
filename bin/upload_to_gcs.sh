15490899f0602375fa1048d88575cd222dc94285
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-01 14:22:38 -0800

    :exclamation: Use ./mksite.sh instead of ./mkdocs.sh

--- bin/upload_to_gcs.sh
+++ bin/upload_to_gcs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo "Usage: ./mkdocs.sh <make cmd> <storage bucket optionally including prefix>"
-echo "Building $1 Documentation ..."
-/bin/sh ./mkdocs.sh "$1"
+echo "Building $1 Site + Docs ..."
+/bin/sh ./mksite.sh "$1"
 echo "Uploading $1 docs to $2 with gsutil ..."
 gsutil cp -r ../doc/sphinx/_build/html "gs://$2"
