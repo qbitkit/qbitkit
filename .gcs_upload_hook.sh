9d494f6b45d192427488d44b3d7922285f79b871
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-01 15:04:01 -0800

    :wrench: Make Sure pip and virtualenv are latest before building

--- .gcs_upload_hook.sh
+++ .gcs_upload_hook.sh
@@ -3,4 +3,6 @@ cd bin
 apt-get update
 apt-get upgrade -y
 apt-get install -y python3-pip python3-virtualenv
+python3 -m pip install -U pip
+python3 -m pip install -U virtualenv
 /bin/sh ./upload_to_gcs.sh "$1" "$2"
