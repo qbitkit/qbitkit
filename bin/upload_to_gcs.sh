bd8fb5265d58a2d540ac78ec97eda372f1fbdc6a
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-01 15:37:56 -0800

    Revert ":wrench: Use rsync to Speed Up Uploads"
    This reverts commit d8b296805bc8c5c69c3da4302e2aafdce0aa3bd4.

--- bin/upload_to_gcs.sh
+++ bin/upload_to_gcs.sh
@@ -3,4 +3,4 @@ echo "Usage: ./mkdocs.sh <make cmd> <storage bucket optionally including prefix>
 echo "Building $1 Site + Docs ..."
 /bin/sh ./mksite.sh "$1"
 echo "Uploading $1 docs to $2 with gsutil ..."
-gsutil -m rsync -r ../doc/sphinx/_build/html "gs://$2"
+gsutil -m cp -r ../doc/sphinx/_build/html "gs://$2"
