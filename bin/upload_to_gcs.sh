e4ee02572ba87bb063d1c8c296c4ed10cd4c5074
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-04 22:07:13 -0800

    :wrench: Remove Virtualenv Dir Before Pushing to GCS
    This should significantly reduce amount of data transferred which should in turn significantly reduce latency and cost.

--- bin/upload_to_gcs.sh
+++ bin/upload_to_gcs.sh
@@ -2,5 +2,7 @@
 echo "Usage: ./mkdocs.sh <make cmd> <storage bucket optionally including prefix>"
 echo "Building $1 Site + Docs ..."
 /bin/sh ./mksite.sh "$1"
+echo "Removing venv directory used to build documentation ..."
+rm -rf ../docs/venv/
 echo "Uploading $1 docs to $2 with gsutil ..."
 gsutil -m rsync -r ../doc/sphinx/_build/html/* "gs://$2"
