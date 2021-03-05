61cab06dd85914f056b6849d9f61a350de497d10
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-04 18:40:15 -0800

    :exclamation: Fix PEP 8 E128, E711, E231, E302, and W292

--- qbitkit/circuit/circuits/basic.py
+++ qbitkit/circuit/circuits/basic.py
@@ -9,16 +9,18 @@ def bell(append=None):
     Returns:
         pandas.DataFrame: Pandas DataFrame containing the Bell State Circuit, or a DataFrame you specified with the Bell State Circuit appended to it."""
     bellFrame = __f__.Frame.get_frame(data={'gate': ['h', 'cnot'],
-                                        'targetA': [0, 0],
-                                        'targetB': [None, 1], })
-    if append == None:
+                                            'targetA': [0, 0],
+                                            'targetB': [None, 1], })
+    if append is None:
         return bellFrame
-    elif append != None:
+    elif append is not None:
         bellFrameAppended = append.append(bellFrame)
         return bellFrameAppended
     else:
         bellFrameAppended = append.append(bellFrame)
         return bellFrameAppended
+
+
 def ghz(append=None):
     """Make a 2-qubit Greenberger-Horne-Zeilinger (GHZ) State Circuit using 1 Hadamard gate and 2 Controlled NOT gates.
 
@@ -26,13 +28,13 @@ def ghz(append=None):
             append(pandas.DataFrame): an existing Pandas DataFrame to append the GHZ State Circuit to. (default None)
         Returns:
             pandas.DataFrame: Pandas DataFrame containing the GHZ State Circuit, or a DataFrame you specified with the GHZ State Circuit appended to it."""
-    ghzFrame = __f__.Frame.get_frame(data={'gate':['h', 'cnot', 'cnot'],
-                                       'targetA':[0,1,2],
-                                       'targetB':[None,1,2],
-                                       'targetC':[None,None,None]})
-    if append == None:
+    ghzFrame = __f__.Frame.get_frame(data={'gate': ['h', 'cnot', 'cnot'],
+                                           'targetA': [0, 1, 2],
+                                           'targetB': [None, 1, 2],
+                                           'targetC': [None, None, None]})
+    if append is None:
         return ghzFrame
-    elif append != None:
+    elif append is not None:
         ghzFrameAppended = append.append(ghzFrame)
         return ghzFrameAppended
     else:
