cf37f1dd62de3f5c229f93d21d606882c7938b46
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-04 22:39:55 -0800

    :bug: :x: Remove Correct Path

--- bin/upload_to_gcs.sh
+++ bin/upload_to_gcs.sh
@@ -3,6 +3,6 @@ echo "Usage: ./mkdocs.sh <make cmd> <storage bucket optionally including prefix>
 echo "Building $1 Site + Docs ..."
 time /bin/sh ./mksite.sh "$1"
 echo "Removing venv directory used to build documentation ..."
-time rm -rf ../docs/venv/
+time rm -rf ../doc/sphinx/_build/html/docs/venv/
 echo "Uploading $1 docs to $2 with gsutil ..."
 time gsutil -m rsync -r ../doc/sphinx/_build/html/ "gs://$2"
