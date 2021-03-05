746fe2dd82c8897cd41ec25aca2fcbd7bba456ec
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-03-04 22:19:25 -0800

    :cloud: Install Time to Measure Timings of Build Steps

--- .gcs_upload_hook.sh
+++ .gcs_upload_hook.sh
@@ -2,7 +2,7 @@
 cd bin
 apt-get update
 apt-get upgrade -y
-apt-get install -y python3-pip python3-virtualenv
+apt-get install -y python3-pip python3-virtualenv time
 python3 -m pip install -U pip
 python3 -m pip install -U virtualenv
 /bin/sh ./upload_to_gcs.sh "$1" "$2"
