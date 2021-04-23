fe95ba9b6e78081d0b74447be73e976ba30c68a0
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-04-23 13:42:14 -0700

    :x: Drop All File I/O Functionality

--- qbitkit/__main__.py
+++ qbitkit/__main__.py
@@ -1,5 +1,4 @@
 import click as ck
-from qbitkit.io.file import csv, json, excel
 from qbitkit.error import error as qbitkit_error
 
 def __init__(self):
@@ -16,22 +15,3 @@ class qbitctl:
         qbitctl.get_support_status()
     def __init__(self):
         qbitctl.get_support_status()
-    class functions:
-        @ck.command()
-        @ck.option('--fileType',
-                   prompt='No File Type Specified.\nFile Type (example: csv, json, excel) :',
-                   help='File type to use when loading data from a file.\nExamples: csv, json, excel')
-        @ck.option('--filePath',
-                   help='Path of file to use when loading data from a file.')
-        def qbitctl(fileType,
-                    filePath):
-            if fileType == 'csv':
-                df = csv.read(data_path=filePath)
-            elif fileType == 'json':
-                df = json.read(data_path=filePath)
-            elif fileType == 'excel':
-                df = excel.read(data_path=filePath)
-            else:
-                err = f"ERROR: File type {fileType} not found.\nTry specifying one of csv, json or excel."
-                print(err)
-            return None
--- qbitkit/io/frame.py
+++ qbitkit/io/frame.py
@@ -1,5 +1,4 @@
 import pandas as __pd__
-import qbitkit.io.file as __f__
 
 
 class Frame:
@@ -14,35 +13,6 @@ class Frame:
         df = __pd__.DataFrame(data=data)
         # Return generated DataFrame.
         return df
-
-    def save_frame(frame=get_frame(),
-                   pth='data.csv',
-                   file_type='csv'):
-        """Save a specified Pandas DataFrame as a specified file type at a specified location.
-
-        Args:
-            frame (pandas.DataFrame): specify the dataframe that we will be saving to disk. (default get_frame())
-            pth (str): specify the path where you want to save the dataframe. (default 'data.csv')
-            file_type (str): what structure (type of file) to use when saving to disk. Can be one of 'csv', 'json', or 'excel'. (default 'csv')
-        Returns:
-            str: path the data was written to"""
-        # Check if the filetype is CSV.
-        if file_type == 'csv':
-            # Write a CSV file to disk.
-            __f__.excel.write(data_path=pth,
-                              df=frame)
-        # Check if the filetype is JSON.
-        if file_type == 'json':
-            # Write a JSON file to disk.
-            __f__.excel.write(data_path=pth,
-                              df=frame)
-        # Check if the filetype is Excel (XLSX).
-        if file_type == 'excel':
-            # Write an Excel file (XLSX) to disk.
-            __f__.excel.write(data_path=pth,
-                              df=frame)
-        # Return the path we wrote the data to.
-        return pth
 
     def fill_nan(self=get_frame(),
                  value=0):
