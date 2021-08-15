7bc23056bcb10fc617f0ef9b2e194534454a1267
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-08-15 10:39:55 -0700

    ➖Drop parameter append for io.list.fill_range

--- qbitkit/io/list.py
+++ qbitkit/io/list.py
@@ -1,6 +1,5 @@
 def fill_range(fill=str('m'),
-               iterations=int(10),
-               append=None):
+               iterations=int(10)):
     """Fill a list with value fill repeated a specified number of times.
 
     Args:
@@ -9,8 +8,7 @@ def fill_range(fill=str('m'),
         append(list): List to append generated list to. If None, does not append to a list. (default None)
     Return:
         list: Generated list."""
-    if type(append) != list:
-        append = []
+    append = []
     for iteration in range(iterations):
         append.append(fill)
     return append
