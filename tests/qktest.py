905c6321a81a3c1638ad77c07e7ecb81d75d07fd
Brian Lechthaler <brianlechthaler@protonmail.ch>
2021-08-14 18:23:38 -0700

    ❗️Fix Typo in QKTestCase.ranger()

--- tests/qktest.py
+++ tests/qktest.py
@@ -7,7 +7,7 @@ from numpy import pi as __pi__
 class QKTestCase(__tc__):
 
     def ranger(span=8):
-        return [number for numbers in range(span)]
+        return [number for number in range(span)]
 
     def repeater(value='x', repeats=8):
         return [value for iteration in range(repeats)]
